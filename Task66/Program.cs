f1f0ac15509bf549b47f9ccf6990f4f01018c72d
Elias Karjalainen <zinkevich-ilia@yandex.ru>
2023-10-27 20:59:47 +0300

    deleted note code - finished

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,21 +1,6 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
-// int M = 1;
-// int N = 15;
-// int printNaturals(int N, int M)
-//     {   
-//         int sum=0;
-//         if(N<M)
-//         {
-//         return 0;
-//         }
-//         sum=sum+N;
-//         printNaturals(N-1, M);
-//         // Console.Write($"{N}\t"); 
-//         return sum;
-//     }
-// Console.Write(printNaturals(N, M));
 
 int M = 4;
 int N = 8;
